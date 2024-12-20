233cca021d41d2fe4d403988b3d0c2af10f20b0b
Abdul Kader <abdulkader0126@gmail.com>
2024-12-20 14:03:10 +0700

    cart and detail

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,9 @@
 <template>
     <nav class="w-full z-20 top-0 start-0">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <RouterLink to="/about"  class="flex items-center space-x-3 rtl:space-x-reverse">
+            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="/asset/image/logo.svg" class="h-24" alt="ayokesitu-logo.png">
-            </RouterLink>
+            </a>
             <div class="flex md:order-2 space-x-3 md:space-x-2 rtl:space-x-reverse">
                 <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
                     class="text-black focus:outline-none font-medium rounded-full text-sm px-5 py-4 text-center bg-[#DBFFEC]"
